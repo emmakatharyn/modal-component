f605352c0fb547657b16746380a46dc8e4023162
Emma Reed <ereed@easitpa.com>
2023-04-12 10:47:18 -0600

    refactor to dry code

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("Button clicked");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-    btnCloseModal.addEventListener("click", function () {
-      modal.classList.add("hidden");
-      overlay.classList.add("hidden");
-    });
-    overlay.addEventListener("click", function () {
-      modal.classList.add("hidden");
-      overlay.classList.add("hidden");
-    });
-  });
+  btnsOpenModal[i].addEventListener("click", openModal);
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
